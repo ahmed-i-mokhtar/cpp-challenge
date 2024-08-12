607d9b201d1709c44cdee0b9c79527470b1f4558
Ahmed Mokhtar <ahmed.mokhtar@getcruise.com>
2024-08-12 03:10:35 -0700

    finished challenge

---
+++ build/CMakeFiles/csv_processor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csv_processor.dir/main.cpp.o"
+  "csv_processor"
+  "csv_processor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/csv_processor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmed.mokhtar/workspaces/user/cpp-challenge
+# Build directory: /home/ahmed.mokhtar/workspaces/user/cpp-challenge/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(CSVProcessorTests "/home/ahmed.mokhtar/workspaces/user/cpp-challenge/build/runTests")
+set_tests_properties(CSVProcessorTests PROPERTIES  _BACKTRACE_TRIPLES "/home/ahmed.mokhtar/workspaces/user/cpp-challenge/CMakeLists.txt;55;add_test;/home/ahmed.mokhtar/workspaces/user/cpp-challenge/CMakeLists.txt;0;")
